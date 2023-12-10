21d333d39f82404d91ae148759486cc22717e44b
João Marcus <85314242+joaomarcus17@users.noreply.github.com>
2023-12-10 09:52:51 -0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -51,3 +51,36 @@ const user= async (req: Request, res: Response) => {
 }
 
 app.post("/addUser", user);
+
+
+//Endepoint Get
+const getUserByName = async (req: Request, res: Response) => {
+  try {
+    const { name } = req.params;
+
+
+    if (!name) {
+      throw new Error("Nome do usuário não fornecido");
+    }
+
+
+    const user = await connection("users").where({ name }).first();
+
+    if (!user) {
+      throw new Error("Usuário não encontrado");
+    }
+
+    // Remova campos sensíveis antes de enviar a resposta (se necessário)
+    const sanitizedUser = {
+      id: user.id,
+      name: user.name,
+      // Adicione outros campos conforme necessário
+    };
+
+    res.status(200).json(sanitizedUser);
+  } catch (error: any) {
+    res.status(500).send(error.sqlMessage || error.message);
+  }
+};
+
+app.get("/getUser/:name", getUserByName);
